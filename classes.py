5661962fa7783bdca500f6de3c0619fe6d5bc7bd
Gontrandddre <gontrandddre@gmail.com>
2019-10-10 20:01:56 +0200

    nettoyage

--- classes.py
+++ classes.py
@@ -1,83 +1,172 @@
-import pygame
-from pygame.locals import * 
-from constantes import *
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+"""
+
+Labyrinth classes
+
+"""
+import random
+
+from constantes import (FILE, NB_SPRITE, SPRITE_SIZE, WINDOW, WALL, DEPARTURE, ARRIVAL, GUARDIAN, MG)
 
 
-class Labyrinthe:
+class Labyrinth:
+	"""
+	This class object 'Labyrinth' allow to define the maze.
+	We have .......
+	"""
 
-	def __init__(self, file):
-		self.file = file
+	def __init__(self):
+		self.file = FILE
 		self.grid = []
+		self.generate()
+		self.x_value = 0
+		self.y_value = 0
 
-
-
-	def generer(self):
-
-		structure = [] # création d'une liste vide
-		with open (self.file, "r") as file.txt: # Ouverture du fichier N1.txt
-			for ligne in n1.txt: # On parcourt chaque ligne
-				for sprite in ligne:
-					structure.append(sprite) # On ajoute pour chaque élément un sprite
+	def generate(self):
+		"""
+		Method which create the structure from the file map/N1.txt.
+		"""
+		structure = []
+		# Create a list of lists.
+		with open(self.file, "r") as file:
+			for ligne in file:
+				to_add = []
+				for character in ligne:
+					if character != '\n':
+						to_add.append(character)
+				structure.append(to_add)
 		self.grid = structure
-					
 
-
-	def afficher(self, screen):
-		
-		mur = pygame.image.load(IMAGE_MUR).convert()
-		depart = pygame.image.load(IMAGE_DEPART).convert()
-		arrivee = pygame.image.load(IMAGE_ARRIVEE).convert_alpha()
-		
-		#On parcourt la liste du niveau
+	def display(self):
+		"""
+		Method wich that allows to put a visual form for our labyrinth:
+		to 'm' an image of a wall;
+		to 'd' an image of a departure;
+		to 'a' an image of a arrival;
+		and to 'g' an image of a guardian.
+		For each sprites in ligne we attribute coordinates (x,y)
+		"""
+		# We start the first list to the first line.
 		num_ligne = 0
-		for ligne in self.file:
-			#On parcourt les listes de lignes
+		for ligne in self.grid:
 			num_sprite = 0
 			for sprite in ligne:
-				#On calcule la position réelle en pixels
-				x = num_sprite * TAILLE_SPRITE
-				y = num_ligne * TAILLE_SPRITE
-				if sprite == 'm':		   #m = Mur
-					screen.blit(mur, (x,y))
-				elif sprite == 'd':		   #d = Départ
-					screen.blit(depart, (x,y))
-				elif sprite == 'a':		   #a = Arrivée
-					screen.blit(arrivee, (x,y))
-				num_case += 1
+				self.x_value = num_sprite * SPRITE_SIZE
+				self.y_value = num_ligne * SPRITE_SIZE
+				if sprite == 'm':		   # m = "Mur".
+					WINDOW.blit(WALL, (self.x_value, self.y_value))
+				elif sprite == 'd':		   # d = "Départ".
+					WINDOW.blit(DEPARTURE, (self.x_value, self.y_value))
+				elif sprite == 'a':		   # a = "Arrivée".
+					WINDOW.blit(ARRIVAL, (self.x_value, self.y_value))
+				elif sprite == 'g':
+					WINDOW.blit(DEPARTURE, (self.x_value, self.y_value))
+					WINDOW.blit(GUARDIAN, (self.x_value, self.y_value))
+				num_sprite += 1
 			num_ligne += 1
 
 
-
-class Perso:
+class Heroe:
+	"""
+	Class wich define a character, with his position (x,y), his picture.
+	This one can move inside the labyrinth except in wall and out of screen.
+	"""
 
 	def __init__(self):
-		self.sprite_x = 0
-		self.sprite_y = 0
-		self.x = 0
-		self.y = 0
+		self.pos_x = 0
+		self.pos_y = 0
+		self.x_value = 0
+		self.y_value = 0
+		self.sprite = MG
 
-	def deplacer (self, direction):
+	def move(self, direction, grid):
+		"""
+		Method wich define the character's abilities to move in the labyrinth.
+		"""
+		# Move to the right.
+		if direction == "right":
+			if self.pos_x < (NB_SPRITE - 1): # To avoid going out of screen.
+				if grid[self.pos_y][self.pos_x+1] != 'm' and grid[self.pos_y][self.pos_x+1] != 'g':
+				# To avoid going out of wall or guardian
+					self.pos_x += 1
+					self.x_value = self.pos_x * SPRITE_SIZE
 
-		if direction == "right": # Tourne à droite s'il n'y a pas de mur à droite
-			if self.file[self.sprite_y][self.sprite_x+1] != 'm':
-				self.sprite_x += 1
-			
-		if direction == "left": # Tourne à gauche s'il n'y a pas de mur à gauche
-			if self.file[self.sprite_y][self.sprite_x-1] != 'm':
-				self.sprite_x -= 1
+		# Move to the left.
+		if direction == "left":
+			if self.pos_x > 0: # To avoid going out of screen.
+				if grid[self.pos_y][self.pos_x-1] != 'm':
+				# To avoid going out of wall.
+					self.pos_x -= 1
+					self.x_value = self.pos_x * SPRITE_SIZE
 
-		if direction == "up": # Tourne en haut s'il n'y a pas de mur en haut
-			if self.file[self.sprite_y-1][self.sprite_x] != 'm':
-				self.sprite_y -= 1
+		# Move to the top.
+		if direction == "top":
+			if self.pos_y > 0: # To avoid going out of screen.
+				if grid[self.pos_y-1][self.pos_x] != 'm':
+				# to avoid going out of wall.
+					self.pos_y -= 1
+					self.y_value = self.pos_y * SPRITE_SIZE
 
-		if direction == "down": # Tourne en bas s'il n'y a pas de mur en bas
-			if self.file[self.sprite_y+1][self.sprite_x] != 'm':
-				self.sprite_y += 1
+		# Move to the bottom.
+		if direction == "bottom":
+			if self.pos_y < (NB_SPRITE - 1): # To avoid going out of screen.
+				if grid[self.pos_y+1][self.pos_x] != 'm':
+				# To avoid going out of wall.
+					self.pos_y += 1
+					self.y_value = self.pos_y * SPRITE_SIZE
+
 
 class Items:
+	"""
+	Class which define 3 items (ether, tube and syringue) with coordinates, images.
+	These items must have random coordinates.
+	These items must be farmed by the main character.
+	"""
 
-	def __init__ (self):
-		self.x = 0
-		self.y = 0
-		self.sprite_x = 0
-		self.sprite_y = 0
+	coordinates_elements = []
+
+	def __init__(self, picture, name):
+		self.pos_x = 0
+		self.pos_y = 0
+		self.x_value = 0
+		self.y_value = 0
+		self.sprite = picture
+		self.name = name
+		self.collected = False
+
+	def locate_items(self, grid):
+		"""
+		Method which define random coordinates for each items.
+		For that we create a dictionnary with all possible coordinates (accessible by the main character).
+		The, we use the function 'randomn' in this list to select coordinates (column,ligne).
+		"""
+		coordinates = ()
+		position = []
+
+		num_ligne = 0
+		while num_ligne < NB_SPRITE:
+			num_column = 1 # We remove the first cell - cell[0] = departure.
+			while num_column < NB_SPRITE:
+				if grid[num_ligne][num_column] == '0': # We select only coordinates in empty cells.
+					coordinates = (num_column, num_ligne)
+					position.append(coordinates)
+				num_column += 1
+			num_ligne += 1
+
+		tmp = random.sample(position, 1)
+		if tmp not in Items.coordinates_elements: # To avoid duplicates coordinates.
+			Items.coordinates_elements.append(tmp)
+
+		self.pos_x, self.pos_y = tmp[0]
+		self.x_value = self.pos_x * SPRITE_SIZE
+		self.y_value = self.pos_y * SPRITE_SIZE
+
+	def farming(self, mgpos):
+		"""
+		Method which define if items are collected by the main character, only if coordinates are equal.
+		"""
+		if mgpos.pos_x == self.pos_x and mgpos.pos_y == self.pos_y:
+			self.collected = True
+			
--- constantes.py
+++ constantes.py
@@ -1,21 +1,89 @@
-"""Constantes du jeu de Labyrinthe MG"""
+#!/usr/bin/python3.5
+# -*-coding:utf-8 -
 
-#Paramètres de la fenêtre
-NOMBRE_SPRITE_COTE = 15
-TAILLE_SPRITE = 30
-COTE_FENETRE = NOMBRE_SPRITE_COTE * TAILLE_SPRITE
+"""
+Labyrinth constants
+"""
 
-#Personnalisation de la fenêtre
-TITRE_FENETRE = "MG Labyrinthe"
-IMAGE_ICONE = "images/dk_droite.png"
+import pygame
 
-#Listes des images du jeu
-IMAGE_FOND = "images/fond.jpg"
-IMAGE_MUR = "images/mur.jpg"
-IMAGE_DEPART = "images/depart.jpg"
-IMAGE_ARRIVEE = "images/arrivee.jpg"
-IMAGE_MACGYVER = "images/macgyver.png"
+pygame.init()
+
+# Dimensions of labyrinth
+NB_SPRITE = 15
+SPRITE_SIZE = 30
+SCREEN_SIDE = NB_SPRITE * SPRITE_SIZE
+
+# Customization window
+WINDOW = pygame.display.set_mode((SCREEN_SIDE + 90, SCREEN_SIDE))
+#... Without this line, 'ICON' will be a misstake.
+#... + 60 px for items scoreboard.
+WINDOW_TITLE = "Help Mac Gyver to escape !"
+ICON = pygame.image.load("images/macgyver.png").convert_alpha()
+
+# Customization window home
+WINDOW_HOME = pygame.Surface((540, 450))
+FONT = pygame.font.Font('freesansbold.ttf', 40)
+SENTENCE1 = "- Mac Gyver Game -"
+SENTENCE2 = "Play  :  press 'SPACE'"
+SENTENCE3 = "QUIT  :  press 'ESCAPE'"
+
+HOME_WINDOW_TITLE1 = FONT.render(SENTENCE1, True, (255, 255, 255))
+HOME_WINDOW_TITLE_RECT1 = HOME_WINDOW_TITLE1.get_rect()
+HOME_WINDOW_TITLE_RECT1.center = (270, 50)
+
+HOME_WINDOW_TITLE2 = FONT.render(SENTENCE2, False, (255, 200, 200))
+HOME_WINDOW_TITLE_RECT2 = HOME_WINDOW_TITLE2.get_rect()
+HOME_WINDOW_TITLE_RECT2.center = (270, 200)
+
+HOME_WINDOW_TITLE3 = FONT.render(SENTENCE3, False, (255, 200, 200))
+HOME_WINDOW_TITLE_RECT3 = HOME_WINDOW_TITLE3.get_rect()
+HOME_WINDOW_TITLE_RECT3.center = (270, 300)
+
+# Customization items window
+FONT = pygame.font.Font('freesansbold.ttf', 10)
+ITEMS_WINDOW_TITLE = FONT.render("Items collected:", True, (255, 255, 255))
+
+SURFACE_ETHER = pygame.Surface((30, 30))
+SURFACE_ETHER.fill((255, 255, 255))
+SURFACE_TUBE = pygame.Surface((30, 30))
+SURFACE_TUBE.fill((255, 255, 255))
+SURFACE_SYRINGUE = pygame.Surface((30, 30))
+SURFACE_SYRINGUE.fill((255, 255, 255))
+
+# Labyrinth elements
+BACKGROUND = pygame.image.load("images/fond.jpg").convert()
+WALL = pygame.image.load("images/mur.jpg").convert()
+WALL = pygame.transform.scale(WALL, (SPRITE_SIZE, SPRITE_SIZE))
+DEPARTURE = pygame.image.load("images/depart.jpg").convert()
+DEPARTURE = pygame.transform.scale(DEPARTURE, (SPRITE_SIZE, SPRITE_SIZE))
+ARRIVAL = pygame.image.load("images/arrivee.jpg").convert()
+ARRIVAL = pygame.transform.scale(ARRIVAL, (SPRITE_SIZE, SPRITE_SIZE))
+
+
+# Labyrinth items
+TUBE = pygame.image.load("images/tube_plastique.png").convert_alpha()
+TUBE = pygame.transform.scale(TUBE, (SPRITE_SIZE, SPRITE_SIZE))
+ETHER = pygame.image.load("images/ether.png").convert_alpha()
+ETHER = pygame.transform.scale(ETHER, (SPRITE_SIZE, SPRITE_SIZE))
+SYRINGUE = pygame.image.load("images/seringue.png").convert_alpha()
+SYRINGUE = pygame.transform.scale(SYRINGUE, (SPRITE_SIZE, SPRITE_SIZE))
+
+# Display characters
+MG = pygame.image.load("images/MacGyver.png").convert_alpha()
+MG = pygame.transform.scale(MG, (SPRITE_SIZE, SPRITE_SIZE))
+GUARDIAN = pygame.image.load("images/Gardien.png").convert_alpha()
+GUARDIAN = pygame.transform.scale(GUARDIAN, (SPRITE_SIZE, SPRITE_SIZE))
+
+# Win & Lose
+WIN = pygame.image.load("images/win.jpg").convert()
+WIN = pygame.transform.scale(WIN, (SCREEN_SIDE + 90, SCREEN_SIDE))
+GAMEOVER = pygame.image.load("images/gameover.png").convert()
+GAMEOVER = pygame.transform.scale(GAMEOVER, (SCREEN_SIDE + 90, SCREEN_SIDE))
 
 # Structure labyrinthe
-
 FILE = "map/N1.txt"
+
+# Sounds
+SOUNDTRACK = pygame.mixer.Sound('sounds/sound_game.wav')
+SOUNDTRACK.set_volume(.5)
